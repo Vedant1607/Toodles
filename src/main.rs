111f88f38bc20042a205c030c68dc4f8ed18deb8
Vedant Sinha <sinhavedant0705@gmail.com>
2026-05-23 02:50:21 +0530

    Initialize basic Ratatui app with event loop and ESC exit handling

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use color_eyre::eyre::{Ok, Result};
+use ratatui::{DefaultTerminal, crossterm::event::{self, Event}};
+
+fn main() -> Result<()> {
+    println!("Hello, world!");
+    color_eyre::install()?;
+
+    let terminal = ratatui::init();
+
+    let result = run(terminal);
+
+    ratatui::restore();
+    result
+}
+
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        // Rendering
+
+        // Input Handling
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(())
+}
